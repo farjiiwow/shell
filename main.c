1bc92c3462ee506b404d9b2c03c0f61218460f0a
farjiiwow <omdewang31122003@gmail.com>
2024-10-27 20:20:02 +0530

    apply REPL cycle

--- main.c
+++ main.c
@@ -2,14 +2,16 @@
 #include <string.h>
 
 int main(void) {
-    printf("$ ");
-    fflush(stdout);
 
     char input[100];
-    fgets(input, 100, stdin);
 
-    input[strlen(input) - 1] = "\0";
-    printf("%s: command not found\n", input);
+    while(1) {
+        printf("farjii:~$ ");
+        fflush(stdout);
+        fgets(input, 100, stdin);
+        input[strlen(input) - 1] = "\0";
+        printf("%s: command not found\n", input);
+    }
 
     return 0;
 }
